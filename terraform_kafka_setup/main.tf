561d6705ed4ff5c5ea68c1914c3c0019fa468377
André Soares <andreghsoares@gmail.com>
2026-01-22 15:44:00 +0000

    add kafka setup - contains errors while applying plan

--- terraform_kafka_setup/main.tf
+++ terraform_kafka_setup/main.tf
@@ -13,10 +13,12 @@ resource "docker_network" "kafka_network" {
   name = "kafka_network"
 }
 
+/*setup zookeeper docker image*/
 resource "docker_image" "zookeeper_image" {
   name = "confluentinc/cp-zookeeper:latest"
 }
 
+#setup zookeeper container
 resource "docker_container" "zookeeper" {
   name  = "zookeeper"
   image = docker_image.zookeeper_image.name
@@ -29,3 +31,27 @@ resource "docker_container" "zookeeper" {
     name = docker_network.kafka_network.name
   }
 }
+
+#setup kafka docker image
+
+resource "docker_image" "kafka_image" {
+  name = "confluentinc/cp-kafka:latest"
+}
+
+resource "docker_container" "kafka" {
+  name  = "kafka"
+  image = docker_image.kafka_image.name
+  ports {
+    internal = 9092
+    external = 9092
+  }
+  env = ["KAFKA_BROKER_ID=1", "KAFKA_ZOOKEEPER_CONNECT=zookeeper:2181",
+    "KAFKA_ADVERTISED_LISTENERS=PLAINTEXT://localhost:9092",
+    "KAFKA_LISTENERS=PLAINTEXT://0.0.0.0:9092",
+  "KAFKA_OFFSETS_TOPIC_REPLICATION_FACTOR=1"]
+
+  networks_advanced {
+    name = docker_network.kafka_network.name
+  }
+}
+
