5c1d2657d12bab71809e8c3ede7835733412e3d3
André Soares <andreghsoares@gmail.com>
2026-03-03 10:30:08 +0000

    adjust setup script

--- aws_setup.sh
+++ aws_setup.sh
@@ -1,4 +1,4 @@
 aws configure set aws_access_key_id "$AWS_ACCESS_KEY_ID" &&
 aws configure set aws_secret_access_key "$AWS_ACCESS_KEY_SECRET" &&
-aws configure set region "$AWS_REGION" --profile user2 && 
+aws configure set region "$AWS_REGION" && 
 aws configure set output "json" 
